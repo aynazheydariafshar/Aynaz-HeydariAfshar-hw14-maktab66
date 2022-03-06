41477bbf57e865aa1d0506e0be61a435fcb268b9
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-03-06 18:56:08 +0330

    add color

--- ex2/src/Components/Color.js
+++ ex2/src/Components/Color.js
@@ -5,6 +5,7 @@ export default class Color extends React.Component{
     render(){
         return(
             <ul>
+                <div style={{backgroundColor : `${this.props.codeColor}`}}></div>
                 <li>{this.props.codeColor}</li>
             </ul>
         )
